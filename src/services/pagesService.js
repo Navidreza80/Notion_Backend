d04cafddccfa86250dda314b908ff388928b952c
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-08-29 13:14:46 +0330

    Fix: Socket.io

--- src/services/pagesService.js
+++ src/services/pagesService.js
@@ -16,6 +16,15 @@ export const updatePage = async (fastify, id, data) => {
   return fastify.prisma.page.update({ where: { id }, data });
 };
 
+export const patchPageContent = async (fastify, id, content) => {
+  return fastify.prisma.page.update({
+    where: { id },
+    data: {
+      content,
+    },
+  });
+};
+
 export const deletePage = async (fastify, id) => {
   return fastify.prisma.page.delete({ where: { id } });
 };
